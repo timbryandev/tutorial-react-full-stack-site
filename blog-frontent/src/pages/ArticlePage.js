8e0c3d30b3b1d7e8d9df9b1cc7de6685cf97b304
Tim Bryan <hi@timbryan.dev>
2020-10-27 12:36:41 +0000

    Added logic for getting and setting article info via API calls

--- blog-frontent/src/pages/ArticlePage.js
+++ blog-frontent/src/pages/ArticlePage.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import articleContent from './article-content';
-import NotFoundPage from './NotFoundPage';
+import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
+import CommentsList from '../components/CommentsList';
+import UpvotesSection from '../components/UpvotesSection';
+import AddCommentFrom from '../components/AddCommentForm';
+import NotFoundPage from './NotFoundPage';
+import articleContent from './article-content';
 
 const ArticlePage = ({ match }) => {
-	const { name } = match.params;
+	const name = match.params.name;
 	const article = articleContent.find(article => article.name === name);
 
-	if (!article) {
-		return <NotFoundPage />;
-	}
+	const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await fetch(`/api/articles/${name}`);
+			const body = await result.json();
+			console.log(body);
+			setArticleInfo(body);
+		};
+		fetchData();
+	}, [name]);
+
+	if (!article) return <NotFoundPage />;
 
 	const otherArticles = articleContent.filter(article => article.name !== name);
 
 	return (
 		<>
 			<h1>{article.title}</h1>
+			<UpvotesSection
+				articleName={name}
+				upvotes={articleInfo.upvotes}
+				setArticleInfo={setArticleInfo}
+			/>
 			{article.content.map((paragraph, key) => (
 				<p key={key}>{paragraph}</p>
 			))}
+			<CommentsList comments={articleInfo.comments} />
+			<AddCommentFrom articleName={name} setArticleInfo={setArticleInfo} />
 			<h3>Other Articles:</h3>
 			<ArticlesList articles={otherArticles} />
 		</>
