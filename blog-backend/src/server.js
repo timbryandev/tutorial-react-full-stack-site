49c8b909063e8ee19fb271cd3c5758b28b52f955
Tim Bryan <hi@timbryan.dev>
2020-10-27 10:54:47 +0000

    Added MongoDB and replaced dummy data with actual DB Added route for grabbing article data from MongoDB

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 
-const articlesInfo = {
-	'learn-react': {
-		upvotes: 0,
-		comments: [],
-	},
-	'learn-node': {
-		upvotes: 0,
-		comments: [],
-	},
-	'my-thoughts-on-resumes': {
-		upvotes: 0,
-		comments: [],
-	},
-};
+require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.get('/api/articles/:name', async (req, res) => {
+	try {
+		const articleName = req.params.name;
+
+		const client = await MongoClient.connect(
+			`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.e1noe.mongodb.net/my-blog`,
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			}
+		);
+		const db = client.db('my-blog');
+
+		const articleInfo = await db
+			.collection('articles')
+			.findOne({ name: articleName });
+		res.status(200).json(articleInfo);
+
+		client.close();
+	} catch (error) {
+		res.status(500).json({ message: 'Error connecting to db', error });
+	}
+});
 
 app.post('/api/articles/:name/upvote', (req, res) => {
 	const articleName = req.params.name;
@@ -41,4 +53,3 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
 });
 
 app.listen(8000, () => console.log('Listening on port 8000'));
-
